783eb6854e4f0bc42c85f3c740f85c9b96e929ff
Christian Aalby Svalesen <christianaalbysvalesen@gmail.com>
2022-10-26 23:39:45 +0200

    added async couroutines

--- async_python/hello_async_coroutines.py
+++ async_python/hello_async_coroutines.py
@@ -1,6 +1,6 @@
 import asyncio 
 import time 
-
+import aiohttp
 
 #hello_async_time.py has been modifid to run coroutines concurretly as asyncio Tasks
 async def say_after(delay, what): 
@@ -9,22 +9,21 @@ async def say_after(delay, what):
 
 async def main(): 
     task1 = asyncio.create_task(
-        say_after(1, 'hello'))
+        say_after(3, 'hello'))
     
     task2 = asyncio.create_task(
-        say_after(2, 'world'))
+        say_after(1, 'world'))
     
     print(f'Started at {time.strftime("%X")}')
 
     #Wati until both tasks are completed (should take around 2 seconds))
-    await task1 
+    await task1
     await task2
 
     print(f'finished at {time.strftime("%X")}')
 
 if __name__=='__main__': 
     asyncio.run(main())
-
 
 
 #Resources: 
